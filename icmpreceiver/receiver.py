df7ef377e5ae25490dd07858192e913f104ffdb5
vguedes <rj.vag.ner@gmail.com>
2016-12-02 21:27:04 -0200

    Adding the janus queue and consuming this queue with treads.

--- icmpreceiver/receiver.py
+++ icmpreceiver/receiver.py
@@ -1,159 +1,32 @@
 #!/usr/bin/python3
+
+import janus
 import socket
 import asyncio
+import threading
 from os import environ
 from struct import unpack
 from datetime import datetime
-from pyzabbix import ZabbixAPI
-from pyzabbix import ZabbixAPIException
 
-q = asyncio.Queue()
-_CONSUMERS = int(environ.get('RECEIVER_TASKS'))
-_ZBX_SERVER = environ.get('ZBX_SERVER')
-_ZBX_USERNAME = environ.get('ZBX_USERNAME')
-_ZBX_PASSWORD = environ.get('ZBX_PASSWORD')
+from zabbix_helppers import (
+    ZabbixAlreadyExistsException,
+    ZabbixHelpper
+)
+
+
+_CONSUMERS = int(environ.get('CONSUMER_TASKS'))
+# _ZBX_BGAN_KEY = environ.get('ZBX_BGAN_KEY')
+# _ZBX_BGAN_ITEM = environ.get('ZBX_BGAN_ITEM')
 _ZBX_BGAN_TEMPLATE = environ.get('ZBX_BGAN_TEMPLATE')
 _ZBX_BGAN_HOSTGROUP = environ.get('ZBX_BGAN_HOSTGROUP')
-_ZBX_BGAN_ITEM = environ.get('ZBX_BGAN_ITEM')
-_ZBX_BGAN_KEY = environ.get('ZBX_BGAN_KEY')
 
 
-class ZabbixNotFoundException(Exception):
-    def __init__(self, message):
-        self.message = message
-
-
-class ZabbixAlreadyExistsException(Exception):
-    def __init__(self, message):
-        self.message = message
-
-
-class ZabbixParameterException(Exception):
-    def __init__(self, message):
-        self.message = message
-
-
-class ZabbixHelpper(object):
-    def __init__(self, group_name=None, template_name=None):
-        self.group_name = group_name
-        self.template_name = template_name
-        self.zapi = ZabbixAPI(_ZBX_SERVER)
-        self.zapi.login(
-            _ZBX_USERNAME,
-            _ZBX_PASSWORD
-        )
-
-    # Get Zabbix group ID by hostgroup name
-    def _getHostgroupId(self, hostgroup_name):
-        hostgroups = self.zapi.hostgroup.get(
-            filter={'name': hostgroup_name}
-        )
-        if not hostgroups:
-            raise ZabbixNotFoundException("Hostgroup not found")
-        return int(hostgroups[0]['groupid'])
-
-    # Get Zabbix template ID by template name
-    def _getTemplateId(self, template_name):
-        templates = self.zapi.template.get(
-            filter={'name': template_name}
-        )
-        if not templates:
-            raise ZabbixNotFoundException("Template not found")
-        return int(templates[0]['templateid'])
-
-    def createHost(self, host_name, ip, group_name=None, template_name=None):
-        """ Create one host in the Zabbix server
-
-        Raises ZabbixNotFound exceptions from:
-            _getTemplateId
-            _getHostgroupId
-
-        Raises ZabbixAlreadyExistsException on atempt to create an existent
-        host (by hostname)
-
-
-        Usage example:
-
-        zbxHelpper = ZabbixHelpper()
-        try:
-            id = zbxHelpper.createHost(
-                'my_host',
-                '10.10.10.10',
-                'my_grp',
-                'my_template'
-            )
-            print(id)
-        except ZabbixAlreadyExistsException as e:
-            print(e.message)
-
-        or setting default group_name and template_name on class initialization
-
-        zbxHelpper = ZabbixHelpper(
-            group_name='my_grp',
-            template_name="my_template"
-        )
-        try:
-            id = zbxHelpper.createHost(
-                'my_host',
-                '10.10.10.10'
-            )
-            print(id)
-        except ZabbixAlreadyExistsException as e:
-            print(e.message)
-
-
-        """
-        if not group_name:
-            if self.group_name:
-                group_name = self.group_name
-            else:
-                raise ZabbixParameterException(
-                    "No group_name given as parameter or"
-                    "on class initialization"
-                )
-        if not template_name:
-            if self.template_name:
-                template_name = self.template_name
-            else:
-                raise ZabbixParameterException(
-                    "No template_name given as parameter or"
-                    "on class initialization"
-                )
-
-        template_id = self._getTemplateId(template_name)
-        host_group_id = self._getHostgroupId(group_name)
-
-        groups = [{'groupid': host_group_id}]
-        templates = [{'templateid': template_id}]
-        interfaces = [{
-            'ip': ip,
-            'useip': 1,
-            "dns": "",
-            "main": 1,
-            "type": 1,
-            "port": "10050"
-        }]
-
-        try:
-            call_rtrn = self.zapi.host.create(
-                groups=groups,
-                host=host_name,
-                inventory_mode=1,
-                templates=templates,
-                interfaces=interfaces
-            )
-            return call_rtrn
-        except ZabbixAPIException as exc:
-            if str(exc).startswith("('Error -32602:"):
-                ex_msg = 'Host %s already exists' % host_name
-                raise ZabbixAlreadyExistsException(ex_msg) from exc
-
-
-async def produce():
-    # c = 0
+async def produce(q):
+    c = 0
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
     s.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
     while True:
+        c += 1
         received = True
         data, addr = s.recvfrom(1058)
         addr = addr[0]
@@ -164,30 +37,61 @@ async def produce():
 
         # 8 is icmp echo request
         if 8 == type:
-            # c += 1
-            # print(c)
             work_queue_item = {
                 "received_addr": addr,
                 "received_datetime": received_time
             }
-            await q.put(work_queue_item)
+            q.async_q.put_nowait(work_queue_item)
+            # q.async_q.join()
+            print("producer sequence: %s" % c)
             await asyncio.sleep(0)
 
 
-async def consume(name):
+def consume(name, q):
+    zbxHelpper = ZabbixHelpper(
+        group_name=_ZBX_BGAN_HOSTGROUP,
+        template_name=_ZBX_BGAN_TEMPLATE
+    )
     c = 0
     while True:
+        val = q.sync_q.get()
+        q.sync_q.task_done()
         c += 1
-        value = await q.get()
-        print(c, name, value)
+        print("consumer %s processed :%s" % (str(name), str(c)))
+        # print('consumer gotten val: ' + str(val))
+        # print(name, str(val))
+        ip_addr = val['received_addr']
+        try:
+            rtrn = zbxHelpper.createHost(
+                ip_addr.replace('.', '_'),
+                ip_addr
+            )
+            print(rtrn)
+        except ZabbixAlreadyExistsException as e:
+            pass
+            # print(e)
 
-loop = asyncio.get_event_loop()
-loop.create_task(produce())
-for x in range(_CONSUMERS):
-    loop.create_task(consume(str(x)))
-try:
-    loop.run_forever()
-except:
-    asyncio.Future.cancel()
-    loop.stop()
-    loop.close()
+
+def run_receiver_forever():
+    loop = asyncio.new_event_loop()
+    q = janus.Queue(loop=loop)
+
+    loop.create_task(produce(q))
+
+    for x in range(_CONSUMERS):
+        t = threading.Thread(
+            target=consume,
+            args=(str(x), q)
+        )
+        t.start()
+
+    try:
+        loop.run_forever()
+    except:
+        asyncio.Future.cancel()
+        loop.stop()
+        loop.close()
+
+
+if __name__ == "__main__":
+    run_receiver_forever()
